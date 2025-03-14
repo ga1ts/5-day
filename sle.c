028c43a846a9c7e1ad36e217c0cb23ffad79f1d6
ga1ts <132671219+ga1ts@users.noreply.github.com>
2025-03-14 11:56:56 +0700

    Update sle.c

--- sle.c
+++ sle.c
@@ -4,98 +4,99 @@
 
 #define EPSILON 1e-8
 
+// Прототипы функций
 int sle(double **matrix, int n, int m, double *roots);
-
-int input(double **matrix, int *n, int *m);
-
-void output(double **matrix, int n, int m);
-
+int input(double **matrix, int n, int m);
 void output_roots(double *roots, int n);
+void free_matrix(double **matrix);
 
+// Главная функция
 int main() {
     double **matrix;
     int n, m;
-    int count = scanf("%d%d", &n, &m);
-    if (count != 2 || n < 1 || m < 1) {
+
+    // Ввод размеров матрицы
+    if (scanf("%d%d", &n, &m) != 2 || n < 1 || m != n + 1) {
         printf("n/a");
         return 0;
     }
-    matrix = malloc(n * m * sizeof(double *) + m * sizeof(double *));
-    double *p = (double *)(matrix + n);
-    for (int i = 0; i < n; ++i) {
-        matrix[i] = p + m * i;
+
+    // Выделение памяти под матрицу
+    matrix = malloc(n * sizeof(double *) + n * (m * sizeof(double)));
+    if (!matrix) {
+        printf("n/a");
+        return 0;
     }
-    count = input(matrix, &n, &m);
-    if (count == 0) {
+    double *p = (double *)(matrix + n);
+    for (int i = 0; i < n; i++) {
+        matrix[i] = p + i * m;
+    }
+
+    // Ввод матрицы
+    if (!input(matrix, n, m)) {
         free(matrix);
         return 0;
     }
+
+    // Выделение памяти под массив корней
     double *roots = (double *)malloc(n * sizeof(double));
-    count = sle(matrix, n, m, roots);
-    if (count == 0) {
+    if (!roots) {
+        printf("n/a");
+        free(matrix);
+        return 0;
+    }
+
+    // Решение СЛАУ
+    if (!sle(matrix, n, m, roots)) {
+        printf("n/a");
         free(matrix);
         free(roots);
-        printf("n/a");
         return 0;
     }
+
+    // Вывод корней и освобождение памяти
     output_roots(roots, n);
     free(matrix);
     free(roots);
+
     return 0;
 }
 
-int input(double **matrix, int *n, int *m) {
-    int count;
-    for (int i = 0; i < *n; ++i) {
-        for (int j = 0; j < *m - 1; j++) {
-            count = scanf("%lf", &matrix[i][j]);
-            if (count != 1) {
+// Чтение матрицы из stdin
+int input(double **matrix, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (scanf("%lf", &matrix[i][j]) != 1) {
                 printf("n/a");
                 return 0;
             }
-        }
-        count = scanf("%lf", &matrix[i][*m - 1]);
-        if (count != 1) {
-            printf("n/a");
-            return 0;
         }
     }
     return 1;
 }
 
-void output(double **matrix, int n, int m) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < m - 1; j++) {
-            printf("%.6lf ", matrix[i][j]);
-        }
-        printf("%.6lf\n", matrix[i][m - 1]);
-    }
-    for (int j = 0; j < m - 1; j++) {
-        printf("%.6lf ", matrix[n - 1][j]);
-    }
-    printf("%.6lf", matrix[n - 1][m - 1]);
-}
-
-void output_roots(double *roots, int n) {
-    for (int i = 0; i < n - 1; ++i) {
-        if (fabs(roots[i]) < EPSILON) {
-            roots[i] = fabs(roots[i]);
-        }
-        printf("%.6lf ", roots[i]);
-    }
-    if (fabs(roots[n - 1]) < EPSILON) {
-        roots[n - 1] = fabs(roots[n - 1]);
-    }
-    printf("%.6lf", roots[n - 1]);
-}
-
 // Решение СЛАУ методом Гаусса
 int sle(double **matrix, int n, int m, double *roots) {
+    // Прямой ход
     for (int i = 0; i < n; i++) {
-        // Приведение матрицы к верхнему треугольному виду
+        // Проверка возможности деления
         if (fabs(matrix[i][i]) < EPSILON) {
-            return 0;  // Матрица вырождена, нет решения
+            int found = 0;
+            for (int k = i + 1; k < n; k++) {
+                if (fabs(matrix[k][i]) > EPSILON) {
+                    // Меняем строки местами
+                    double *temp = matrix[i];
+                    matrix[i] = matrix[k];
+                    matrix[k] = temp;
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) // Если нельзя найти ненулевой элемент
+                return 0;
         }
+
+        // Приведение матрицы к треугольному виду
         for (int k = i + 1; k < n; k++) {
             double factor = matrix[k][i] / matrix[i][i];
             for (int j = i; j < m; j++) {
@@ -104,7 +105,7 @@ int sle(double **matrix, int n, int m, double *roots) {
         }
     }
 
-    // Обратный ход, нахождение корней
+    // Обратный ход
     for (int i = n - 1; i >= 0; i--) {
         double sum = 0.0;
         for (int j = i + 1; j < n; j++) {
@@ -114,4 +115,19 @@ int sle(double **matrix, int n, int m, double *roots) {
     }
 
     return 1;
+}
+
+// Вывод решений
+void output_roots(double *roots, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%.6lf", roots[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+}
+
+// Освобождение памяти (если понадобится расширять код)
+void free_matrix(double **matrix) {
+    free(matrix);
 }
